388bbb518d4e5a4fcfbf5fd023fd5d203a355b1d
sanjo <santiago11200311@gmail.com>
2025-07-11 14:20:05 -0500

    Creando un saludo a Github desde .cpp en Acode

---
+++ prueba_cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+void Saludar_Github(std::string saludo){
+  std::cout << saludo << std::endl;
+}
+
+int main(){
+  Saludar_Github("¡Hola, Github! Desde Termux Android");
+}
